8b547133240f02e110bc55b6178f52b2aee2bbd7
Joe Cho <joe@agioskatastrof.com>
2015-08-19 17:59:15 -0400

    .

--- BankOfDad.js
+++ BankOfDad.js
@@ -4,7 +4,7 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
 
@@ -26,4 +26,15 @@ if (Meteor.isClient) {
       event.target.text.value = "";
     }
   });
+  
+  Template.task.events({
+    "click .toggle-checked": function () {
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function() {
+      Tasks.remove(this._id);
+    }
+  })
 }
