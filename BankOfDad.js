2b76059c5744f721ff55456e59dbdd73b487cc34
Joe Cho <joe@agioskatastrof.com>
2015-08-19 17:33:53 -0400

    . 	# Please enter the commit message for your changes. Lines starting

--- BankOfDad.js
+++ BankOfDad.js
@@ -1,10 +1,29 @@
+Tasks = new Mongo.Collection("tasks");
+ 
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
+    tasks: function () {
+      return Tasks.find({});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+ 
+      // Get value from form element
+      var text = event.target.text.value;
+ 
+      // Insert a task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+ 
+      // Clear form
+      event.target.text.value = "";
+    }
   });
 }
