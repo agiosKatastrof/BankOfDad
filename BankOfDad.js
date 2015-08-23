15a51465a2a5aba6917b7c7a81b7fc94b7af5bb9
Joe Cho <joe@agioskatastrof.com>
2015-08-23 16:10:40 -0400

    .

--- BankOfDad.js
+++ BankOfDad.js
@@ -41,13 +41,21 @@ if (Meteor.isClient) {
     }
   });
   
-  Template.body.helpers({
+ Template.body.helpers({
     tasks: function () {
       if (Session.get("hideCompleted")) {
-        return Tasks.find({checked: {$ne: $true}}, {sort: {createdAt: -1}});
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
       } else {
+        // Otherwise, return all of the tasks
         return Tasks.find({}, {sort: {createdAt: -1}});
       }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
-  })
+  });
 }
